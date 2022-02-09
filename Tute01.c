7ff4852490a487b83163f142bef8d901291a319d
IT21343766 <99312122+IT21343766@users.noreply.github.com>
2022-02-09 06:10:30 +0000

    Completed.

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,22 @@
 
 #include <stdio.h>
 
-int main() {
-  
+int main() 
+{
+  // Variable declaration and initialization
+  float marks = 0 , avg = 0 ;
+
+  //Getting the inputs
+  for (int i = 0 ; i < 2 ; i++)
+  {
+    printf("Enter marks for subject %d : " , i+1) ;
+    scanf("%f" , &marks ) ;
+    avg += marks ;  //Getting the sum of marks
+  }
+  avg /= 2 ; // converting the sum to average
+
+  // Display results and termination
+  printf("\nThe average of the two marks : %.2f" , avg) ;
   return 0;
 }
 
